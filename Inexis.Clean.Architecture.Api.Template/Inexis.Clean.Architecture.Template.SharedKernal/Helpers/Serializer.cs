b71473226aea68065ca85957648f4211536946e0
Thanzeel Jalaldeen <thanzeel@inexisconsulting.com>
2023-04-03 14:07:03 +0530

    added helper methods to shared kernal

---
+++ Inexis.Clean.Architecture.Api.Template/Inexis.Clean.Architecture.Template.SharedKernal/Helpers/Serializer.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace Inexis.Clean.Architecture.Template.SharedKernal.Helpers;
+
+public static class Serializer
+{
+    public static string Serialize(object instance)
+    {
+        var serializedData = JsonConvert.SerializeObject(instance, new JsonSerializerSettings
+        {
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+            Converters = new JsonConverter[] { new StringEnumConverter() }
+        });
+
+        return serializedData;
+    }
+}
