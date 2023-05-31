9ef19a632430e226d3215d6684728685d9d94f13
Thanzeel Jalaldeen <tnlthanzeel@gmail.com>
2023-05-31 11:33:01 +0530

    automapper mapping issue fixed

---
+++ Inexis.Clean.Architecture.Api.Template/Inexis.Clean.Architecture.Template.Application/AutomapperProfiles/UserMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Inexis.Clean.Architecture.Template.Application.Security.Dtos;
+using Inexis.Clean.Architecture.Template.Domain.Entities.IdentityUserEntities;
+
+namespace Inexis.Clean.Architecture.Template.Application.AutomapperProfiles;
+
+public sealed class UserMappingProfile : Profile
+{
+    public UserMappingProfile()
+    {
+        CreateMap<ApplicationUser, UserSummaryDto>()
+            .ForMember(d => d.FirstName, s => s.MapFrom(src => src.UserProfile.FirstName))
+            .ForMember(d => d.LastName, s => s.MapFrom(src => src.UserProfile.LastName));
+    }
+}
