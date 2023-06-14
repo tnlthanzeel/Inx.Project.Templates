f352d8dc72816c6d8b55ade3cc5847fba4c91af4
Thanzeel Jalaldeen <tnlthanzeel@gmail.com>
2023-06-14 13:02:39 +0530

    changed to file scoped namespace

--- Inexis.Clean.Architecture.Api.Template/Inexis.Clean.Architecture.Template.Core/Security/JwtConfig.cs
+++ Inexis.Clean.Architecture.Api.Template/Inexis.Clean.Architecture.Template.Core/Security/JwtConfig.cs
@@ -1,10 +1,9 @@
-﻿namespace Inexis.Clean.Architecture.Template.Core.Security
+﻿namespace Inexis.Clean.Architecture.Template.Core.Security;
+
+public sealed class JwtConfig
 {
-    public sealed class JwtConfig
-    {
-        public string Issuer { get; set; } = null!;
-        public string Audience { get; set; } = null!;
-        public string SigningKey { get; set; } = null!;
-        public TimeSpan TokenLifetime { get; set; }
-    }
+    public string Issuer { get; set; } = null!;
+    public string Audience { get; set; } = null!;
+    public string SigningKey { get; set; } = null!;
+    public TimeSpan TokenLifetime { get; set; }
 }
