851c42e5e7e7b7ae1b918f3a983fff530230a0f9
Thanzeel Jalaldeen <tnlthanzeel@gmail.com>
2023-06-19 13:19:26 +0530

    user profile table updated

--- Inexis.Clean.Architecture.Api.Template/Inexis.Clean.Architecture.Template.Core/Security/Entities/UserProfile.cs
+++ Inexis.Clean.Architecture.Api.Template/Inexis.Clean.Architecture.Template.Core/Security/Entities/UserProfile.cs
@@ -1,7 +1,18 @@
-﻿namespace Inexis.Clean.Architecture.Template.Core.Security.Entities;
+﻿using Inexis.Clean.Architecture.Template.SharedKernal.Interfaces;
 
-public sealed class UserProfile
+namespace Inexis.Clean.Architecture.Template.Core.Security.Entities;
+
+public sealed class UserProfile : ICreatedAudit, IUpdatedAudit, IDeletedAudit
 {
+
+    public string? CreatedBy { get; set; }
+    public DateTimeOffset CreatedOn { get; set; } = DateTimeOffset.UtcNow;
+    public DateTimeOffset? UpdatedOn { get; set; }
+    public string? UpdatedBy { get; set; }
+    public DateTimeOffset? DeletedOn { get; set; }
+    public string? DeletedBy { get; set; }
+    public bool IsDeleted { get; private set; }
+
     public Guid Id { get; set; }
 
     public string FirstName { get; set; } = null!;
--- Inexis.Clean.Architecture.Api.Template/Inexis.Clean.Architecture.Template.Persistence/Configurations/UserConfigs/UserProfileConfig.cs
+++ Inexis.Clean.Architecture.Api.Template/Inexis.Clean.Architecture.Template.Persistence/Configurations/UserConfigs/UserProfileConfig.cs
@@ -9,6 +9,8 @@ internal sealed class UserProfileConfig : IEntityTypeConfiguration<UserProfile>
 {
     public void Configure(EntityTypeBuilder<UserProfile> builder)
     {
+        builder.HasQueryFilter(w => w.IsDeleted == false);
+
         builder.Property(p => p.FirstName).IsRequired().HasMaxLength(AppConstants.StringLengths.FirstName);
 
         builder.Property(p => p.LastName).IsRequired().HasMaxLength(AppConstants.StringLengths.LastName);
@@ -20,7 +22,8 @@ internal sealed class UserProfileConfig : IEntityTypeConfiguration<UserProfile>
             Id = AppConstants.SuperAdmin.SuperUserId,
             FirstName = "Super",
             LastName = "Admin",
-            TimeZone = "Sri Lanka Standard Time"
+            TimeZone = "Sri Lanka Standard Time",
+            CreatedOn = new DateTimeOffset(new DateTime(2022, 6, 30)),
         });
     }
 }
