cdf91b10c707a6148e15b46a4916c8647cb5b81c
Thanzeel Jalaldeen <tnlthanzeel@gmail.com>
2023-10-25 17:02:11 +0530

    azure funcation app - email nofirication - refactored

--- Inexis.Clean.Architecture.Api.Template/Inexis.Clean.Architecture.Template.FunctionApp.CP/NotificationFunctions/EmailNotificationFunction.cs
+++ Inexis.Clean.Architecture.Api.Template/Inexis.Clean.Architecture.Template.FunctionApp.CP/NotificationFunctions/EmailNotificationFunction.cs
@@ -9,14 +9,19 @@ using MimeKit.Text;
 
 namespace Inexis.Clean.Architecture.Template.FunctionApp.CP.NotificationFunctions;
 
-public class EmailNotificationFunction
+public sealed class EmailNotificationFunction
 {
     [Function(nameof(SendEmailFunc))]
     public static async Task SendEmailFunc([QueueTrigger(AppConstants.QueueStorage.QueueName.EmailQueue)] EmailMessage emailMessage)
     {
+        string smtpEmailAddress = Environment.GetEnvironmentVariable("SMTPEmailAddress")!;
+        string smtpEmailHost = Environment.GetEnvironmentVariable("SMTPEmailHost")!;
+        string smtpEmailUserName = Environment.GetEnvironmentVariable("SMTPEmailUserName")!;
+        string smtpEmailPassword = Environment.GetEnvironmentVariable("SMTPEmailPassword")!;
+
         MimeMessage message = new();
 
-        message.From.Add(MailboxAddress.Parse("<smpt enabled email>"));
+        message.From.Add(MailboxAddress.Parse(smtpEmailAddress));
 
         message.To.Add(MailboxAddress.Parse(emailMessage.To));
 
@@ -28,8 +33,8 @@ public class EmailNotificationFunction
         };
         using var client = new SmtpClient();
 
-        await client.ConnectAsync("<host>", 587, SecureSocketOptions.StartTls);
-        await client.AuthenticateAsync("<username>", "<password>");
+        await client.ConnectAsync(smtpEmailHost, 587, SecureSocketOptions.StartTls);
+        await client.AuthenticateAsync(smtpEmailUserName, smtpEmailPassword);
 
         await client.SendAsync(message);
         await client.DisconnectAsync(true);
