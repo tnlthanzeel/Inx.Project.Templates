40a49f1bb8f4cb2231201aa649af4f54c40ee794
Thanzeel Jalaldeen <tnlthanzeel@gmail.com>
2023-12-19 12:52:47 +0530

    code cleanup

--- Inexis.Clean.Architecture.Api.Template/Inexis.Clean.Architecture.Template.Api/ServiceRegistrations/IdentityConfig.cs
+++ Inexis.Clean.Architecture.Api.Template/Inexis.Clean.Architecture.Template.Api/ServiceRegistrations/IdentityConfig.cs
@@ -108,8 +108,7 @@ internal static class IdentityConfig
         {
             var authPolicyApplicators = typeof(Program).Assembly
                                                        .ExportedTypes
-                                                       .Where(x => typeof(IAuthPolicyApplyer)
-                                                                   .IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract)
+                                                       .Where(x => typeof(IAuthPolicyApplyer).IsAssignableFrom(x) && !x.IsInterface && !x.IsAbstract)
                                                        .Select(Activator.CreateInstance)
                                                        .Cast<IAuthPolicyApplyer>()
                                                        .ToList();
