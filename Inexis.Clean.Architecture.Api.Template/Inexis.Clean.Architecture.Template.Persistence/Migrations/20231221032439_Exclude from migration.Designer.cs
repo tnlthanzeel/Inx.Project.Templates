647cc66d41a19fa0f50273c530415358a8de1bda
Thanzeel Jalaldeen <tnlthanzeel@gmail.com>
2023-12-21 08:56:14 +0530

    default user password changed

--- Inexis.Clean.Architecture.Api.Template/Inexis.Clean.Architecture.Template.Persistence/Migrations/20231221032439_Exclude from migration.Designer.cs
+++ Inexis.Clean.Architecture.Api.Template/Inexis.Clean.Architecture.Template.Persistence/Migrations/20231221032439_Exclude from migration.Designer.cs
@@ -4,6 +4,7 @@ using Inexis.Clean.Architecture.Template.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Inexis.Clean.Architecture.Template.Persistence.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231221032439_Exclude from migration")]
+    partial class Excludefrommigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
